88390cac8a34bd0bbb1609f5ac1dfe0ee3becc78
CARTER_CLEMENTS <831@holbertonschool.com>
2019-11-12 15:22:11 -0500

    Added more commands, still working on functionality

--- console.py
+++ console.py
@@ -2,7 +2,8 @@
 """ console file """
 
 import cmd
-
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 class HBNBCommand(cmd.Cmd):
     """ HBNBCommand -
@@ -24,6 +25,37 @@ class HBNBCommand(cmd.Cmd):
 
     # ------ Commands ------
 
+    def do_create(self, arg):
+        """ do_create -
+
+            Creates a new instance of BaseModel,
+            saves it (To the JSON file) and prints
+            the id
+            Ex. create BaseModel
+        """
+        if len(arg) is 0:
+            print("** class name missing **")
+            return
+
+        if arg == "BaseModel":
+            new_bmodel = BaseModel()
+            new_bmodel.save()
+
+            print(new_bmodel.id)
+        else:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """ do_show -
+
+            Prints the string representation of an
+            instance base on the class name and id
+        """
+        if len(arg) is 0:
+            print("**class name missing**")
+            return
+
+
     def do_quit(self, arg):
         """ do_quit -
 
@@ -42,13 +74,72 @@ class HBNBCommand(cmd.Cmd):
 
     # ----- Documentation/help commands -----
 
+    def help_update(self):
+        """ help_update -
+
+           prints ouf the basic usage
+           and description of the
+           update command
+        """
+
+        print("\nUpdates an instance based on the class name")
+        print("and id by adding or update an attribute")
+        attrib = "1234-1234-1234 email \"aibnb@holbertonschool.com\""
+        print("-\nUsage: (hbnb) update Basemodel {}\n".format(attrib))
+
+    def help_all(self):
+        """ help_all -
+
+            prints out the basic usage
+            and description of the
+            all command
+        """
+        print("\nPrints all string representation of all instances")
+        print("based or not on the class name")
+        print("-\nUsage: (hbnb) all BaseModel")
+        print("or (hbnb) all\n")
+
+
+    def help_destroy(self):
+        """ help_destroy -
+
+            prints out the basic usage
+            and description of the
+            destroy command
+        """
+        print("\nDeletes an instance based on the class name")
+        print("and id (saves change to into the JSON file")
+        print("-\nUsage: destroy BaseModel 1234-1234-1234\n")
+
+    def help_show(self):
+        """ help_show -
+
+            prints out the basic usage
+            and description of the
+            show command
+        """
+        print("\nPrints the string representation of an instance")
+        print("based on the class name and id")
+        print("-\nUsage: (hbnb) show BaseModel 1234-1234-1234\n")
+
+    def help_create(self):
+        """ help_create -
+
+            prints out basic usage of
+            the create command
+        """
+        print("\nCreates a new instance of BaseModel,")
+        print("saves it (to the JSON file) and prints the id.")
+        print("- \nUsage: (hbnb) create BaseModel\n")
+
     def help_quit(self):
         """ help_quit -
 
             Prints out basic usage of the
             Quit command
         """
-        print("Quit command to exit the program\n")
+        print("\nQuit command to exit the program")
+        print("Usage: (hbnb) quit\n")
 
     def help_EOF(self):
         """ help_EOF -
@@ -56,8 +147,22 @@ class HBNBCommand(cmd.Cmd):
             Prints out basic usage of the
             EOF command
         """
-        print("EOF command to exit the program\n")
+        print("\nEOF command to exit the program")
+        print("Usage: (hbnb) EOF\n")
 
+    # ----- Overrides n various other useful stuff ----
+
+    def emptyline(self):
+        """ emptyline -
+
+            this function just overrides the
+            predefined emptyline function. The
+            override prevents the console from
+            running the previous command entered
+            when the user inputs a blank line
+        """
+
+        return ""
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop(HBNBCommand().intro_prompt)
