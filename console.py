9e7111f9e10179eac00b19cf4058e5eb920d285d
CARTER_CLEMENTS <831@holbertonschool.com>
2019-11-13 23:42:19 -0500

    Added more classes to import

--- console.py
+++ console.py
@@ -6,7 +6,12 @@ import cmd
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.engine.file_storage import FileStorage
-
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 class HBNBCommand(cmd.Cmd):
     """ HBNBCommand -
