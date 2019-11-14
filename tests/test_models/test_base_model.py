033ea795914f75fe027605a88034044ac316bd5e
Nathaniel821 <nathaniel.marofsky@gmail.com>
2019-11-13 21:51:05 -0500

    add more tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -11,8 +11,13 @@ class TestBaseModel(unittest.TestCase):
     """ test basemodel """
 
     testing_class = BaseModel
+    template = {}
 
     def test_create(self):
+        """
+        see if making instances
+        basically works, mostly
+        """
         x = self.testing_class()
         self.assertIsInstance(x, self.testing_class)
         self.assertIsInstance(x.id, str)
@@ -20,7 +25,23 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(x.updated_at, datetime)
 
     def test_dict(self):
+        """
+        make sure to_dict works,
+        and that the data can be
+        restored from this form.
+        """
         x = self.testing_class()
         xd = x.to_dict()
         y = self.testing_class(**xd)
         self.assertEqual(xd, y.to_dict())
+
+    def test_attr(self):
+        """
+        This tests that each sub
+        class has all its proper
+        attributes, and that the
+        default values are right
+        """
+        x = self.testing_class()
+        for name in self.template:
+            self.assertEqual(getattr(x, name), self.template[name])
--- tests/test_models/test_others.py
+++ tests/test_models/test_others.py
@@ -15,30 +15,61 @@ class TestUser(TestBaseModel):
     """ test user"""
 
     testing_class = User
+    template = {
+        "email": "",
+        "password": "",
+        "first_name": "",
+        "last_name": ""
+    }
 
 
 class TestAmenity(TestBaseModel):
     """ test """
 
     testing_class = Amenity
+    template = {
+        "name": ""
+    }
 
 
 class TestCity(TestBaseModel):
     """ test """
 
     testing_class = City
+    template = {
+        "state_id": "",
+        "name": ""
+    }
 
 
 class TestPlace(TestBaseModel):
     """ test """
 
     testing_class = Place
+    template = {
+        "city_id": "",
+        "user_id": "",
+        "name": "",
+        "description": "",
+        "number_rooms": 0,
+        "number_bathrooms": 0,
+        "max_guest": 0,
+        "price_by_night": 0,
+        "latitude": 0.0,
+        "longitude": 0.0,
+        "amenity_ids": [],
+    }
 
 
 class TestReview(TestBaseModel):
     """ test """
 
     testing_class = Review
+    template = {
+        "place_id": "",
+        "user_id": "",
+        "text": ""
+    }
 
     def test_spelling(self):
         """ make sure I spell review correctly
@@ -52,3 +83,6 @@ class TestState(TestBaseModel):
     """ test """
 
     testing_class = State
+    template = {
+        "name": ""
+    }
